5655b0c6b97be445be57cefe11023f2aaf2db463
Enmanuel Tejeda <Enmanuel.etg@gmail.com>
2023-04-14 15:06:48 -0400

    h

--- streamlit_app.py
+++ streamlit_app.py
@@ -257,31 +257,39 @@ def train():
 
 
 
+@st.cache
+def train1():
 
-trainData = pd.read_csv("train.csv")
-testData = pd.read_csv("test.csv")
-
-
-
-trainData = trainData.drop('Id', axis=1)
-df_num = trainData.select_dtypes(include=['float64', 'int64'])
-df_num = df_num[np.isfinite(df_num).all(1)]
-X_train = df_num.drop("SalePrice", axis=1)
-y_train = df_num["SalePrice"]
+    trainData = pd.read_csv("train.csv")
+    testData = pd.read_csv("test.csv")
 
 
-model = sklearn.linear_model.LinearRegression()
-model.fit(X_train, y_train)
+    trainData = trainData.drop('Id', axis=1)
+    df_num = trainData.select_dtypes(include=['float64', 'int64'])
+    df_num = df_num[np.isfinite(df_num).all(1)]
+    X_train = df_num.drop("SalePrice", axis=1)
+    y_train = df_num["SalePrice"]
 
 
-testData = testData.drop('Id', axis=1)
-X_test = testData.select_dtypes(include=['float64', 'int64'])
-X_test = X_test[np.isfinite(X_test).all(1)]
+    model = sklearn.linear_model.LinearRegression()
+    model.fit(X_train, y_train)
 
 
-explainer = shap.Explainer(model.predict, X_test)
-shap_values = explainer(X_test)
-st_shap(shap.summary_plot(shap_values, plot_type='violin'))
+    testData = testData.drop('Id', axis=1)
+    X_test = testData.select_dtypes(include=['float64', 'int64'])
+    X_test = X_test[np.isfinite(X_test).all(1)]
+
+
+    explainer = shap.Explainer(model.predict, X_test)
+    shap_values = explainer(X_test)
+    st_shap(shap.summary_plot(shap_values, plot_type='violin'))
+    return none
+
+
+train1()
+
+
+
 
 
 
@@ -289,25 +297,8 @@ st_shap(shap.summary_plot(shap_values, plot_type='violin'))
 gbm = train()
 def test():
     df = pd.DataFrame(mydict)
-
-
-    # ,predict_disable_shape_check=True
-    # print(gbm.predict(df, num_iteration=gbm.best_iteration))
-    #st.write(gbm.predict(df, num_iteration=gbm.best_iteration));
     desired_representation = "{:0,.4f}".format(gbm.predict(df, num_iteration=gbm.best_iteration)[0])
     st.write(desired_representation);
-
-    #displaySHAP();
-#     testData = testData.drop('Id', axis=1)
-#     X_test = testData.select_dtypes(include=['float64', 'int64'])
-#     X_test = X_test[np.isfinite(X_test).all(1)]
-
-
-#     explainer = shap.Explainer(gbm.predict, X_test)
-#     shap_values = explainer(X_test)
-
-#     #st_shap(shap.summary_plot(shap_values), height=1080)
-#     st_shap(shap.summary_plot(shap_values, plot_type='violin'), height=1080)
 
 
 
