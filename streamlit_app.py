9768e7d099a69437e6f126111925514de8bed29b
Enmanuel Tejeda <Enmanuel.etg@gmail.com>
2023-04-14 15:28:54 -0400

    kj

--- streamlit_app.py
+++ streamlit_app.py
@@ -302,7 +302,7 @@ st.button("sup",on_click=test)
 val = train1()
 
 #st_shap(shap.summary_plot(val, plot_type='interaction'))
-st_shap(shap.summary_plot(val, plot_type='violin'), height = 1080)
+st_shap(shap.summary_plot(val, plot_type='violin'))
 
 
 
