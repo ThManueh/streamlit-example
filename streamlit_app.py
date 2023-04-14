a329900a6c0ca30891a8373ff4ecff2f8012c1b6
Enmanuel Tejeda <Enmanuel.etg@gmail.com>
2023-04-14 15:02:38 -0400

    h

--- streamlit_app.py
+++ streamlit_app.py
@@ -257,40 +257,37 @@ def train():
 
 
 
-@st.cache
-def displaySHAP():
-    trainData = pd.read_csv("train.csv")
-    testData = pd.read_csv("test.csv")
+
+trainData = pd.read_csv("train.csv")
+testData = pd.read_csv("test.csv")
 
 
 
-    trainData = trainData.drop('Id', axis=1)
-    df_num = trainData.select_dtypes(include=['float64', 'int64'])
-    df_num = df_num[np.isfinite(df_num).all(1)]
-    X_train = df_num.drop("SalePrice", axis=1)
-    y_train = df_num["SalePrice"]
+trainData = trainData.drop('Id', axis=1)
+df_num = trainData.select_dtypes(include=['float64', 'int64'])
+df_num = df_num[np.isfinite(df_num).all(1)]
+X_train = df_num.drop("SalePrice", axis=1)
+y_train = df_num["SalePrice"]
 
 
-    model = sklearn.linear_model.LinearRegression()
-    model.fit(X_train, y_train)
+model = sklearn.linear_model.LinearRegression()
+model.fit(X_train, y_train)
 
 
-    testData = testData.drop('Id', axis=1)
-    X_test = testData.select_dtypes(include=['float64', 'int64'])
-    X_test = X_test[np.isfinite(X_test).all(1)]
+testData = testData.drop('Id', axis=1)
+X_test = testData.select_dtypes(include=['float64', 'int64'])
+X_test = X_test[np.isfinite(X_test).all(1)]
 
 
-    explainer = shap.Explainer(model.predict, X_test)
-    shap_values = explainer(X_test)
-    st_shap(shap.summary_plot(shap_values, plot_type='violin'))
+explainer = shap.Explainer(model.predict, X_test)
+shap_values = explainer(X_test)
+st_shap(shap.summary_plot(shap_values, plot_type='violin'))
 
 
 
 
 gbm = train()
 def test():
-
-
     df = pd.DataFrame(mydict)
 
 
@@ -300,7 +297,7 @@ def test():
     desired_representation = "{:0,.4f}".format(gbm.predict(df, num_iteration=gbm.best_iteration)[0])
     st.write(desired_representation);
 
-    displaySHAP();
+    #displaySHAP();
 #     testData = testData.drop('Id', axis=1)
 #     X_test = testData.select_dtypes(include=['float64', 'int64'])
 #     X_test = X_test[np.isfinite(X_test).all(1)]
